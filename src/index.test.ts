ea6eecc8a9143bda348330bd16645ac2703b5ecc
Naitik Shah <n@daaku.org>
2024-03-11 10:46:10 +0400

    move code into real location

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,15 @@
 import { expect, test } from 'bun:test'
 import { Database } from 'bun:sqlite'
-import { uuidv7 } from '@daaku/uuidv7'
-import memize from 'memize'
+import {
+  $toData,
+  all,
+  get,
+  getByID,
+  insert,
+  qCreateIndex,
+  qCreateTable,
+  sql,
+} from './index.js'
 
 const expectIndexOp = (db: Database, sql: string, ...args: any[]) => {
   const explain = db.query(sql)
@@ -16,109 +24,8 @@ test('crud', async () => {
     gender?: 'male' | 'female'
   }
 
-  // create table
-  // create indexes
-  // insert document
-  // one document
-  // insert bulk
-  // all document
   // update document
   // delete document
-
-  const qCreateTable = memize(
-    (name: string): string => `create table if not exists ${name} (data text)`,
-  )
-
-  const exprToName = memize((expr: string): string =>
-    expr.replace(/[^a-zA-Z]+/g, '_').replace(/_$/, ''),
-  )
-
-  interface CreateIndex {
-    table: string
-    expr: string
-    name?: string
-    unique?: boolean
-  }
-  const qCreateIndex = memize((o: CreateIndex): string => {
-    return [
-      'create ',
-      o.unique ? 'unique ' : '',
-      'index if not exists ',
-      o.name ?? `${o.table}_${exprToName(o.expr)}`,
-      ' on ',
-      o.table,
-      ' (',
-      o.expr,
-      ')',
-    ].join('')
-  })
-
-  const qInsert = memize(
-    (table: string): string => `insert into ${table} (data) values (?)`,
-  )
-
-  const insert = <T extends Object>(
-    db: Database,
-    table: string,
-    doc: T,
-  ): T & { id: string } => {
-    // @ts-ignore muck with id
-    if (!doc.id) {
-      doc = { ...doc, id: uuidv7() }
-    }
-    const sql = qInsert(table)
-    const stmt = db.query<undefined, string>(sql)
-    stmt.run(JSON.stringify(doc))
-    // @ts-ignore muck with id
-    return doc
-  }
-
-  const $toData = memize((s: string): string =>
-    s.replace(/\$([A-Za-z_\.]+)/g, `data->>'$1'`),
-  )
-
-  const $arrayToData = memize((vs: readonly string[]): string[] =>
-    vs.map($toData),
-  )
-
-  interface SQLParts {
-    parts: readonly string[]
-    values: any[]
-  }
-  const sql = (strings: TemplateStringsArray, ...values: any[]): SQLParts => {
-    return {
-      parts: $arrayToData(strings),
-      values: values,
-    }
-  }
-
-  const all = <Doc = unknown>(
-    db: Database,
-    table: string,
-    ...sqls: SQLParts[]
-  ): Doc[] => {
-    const sql = [
-      'select data from',
-      table,
-      ...sqls.map(v => v.parts.join('?')),
-    ].join(' ')
-    const args = sqls.flatMap(v => v.values)
-    const stmt = db.query<{ data: string }, any[]>(sql)
-    // @ts-ignore we expect [string][]
-    return stmt.values(...args).flatMap(JSON.parse)
-  }
-
-  const get = <Doc = unknown>(
-    db: Database,
-    table: string,
-    ...sqls: SQLParts[]
-  ): Doc | undefined => all<Doc>(db, table, ...sqls, sql`limit 1`)[0]
-
-  const getByID = <Doc = unknown>(
-    db: Database,
-    table: string,
-    id: string,
-  ): Doc | undefined => get<Doc>(db, table, sql`where $id = ${id}`)
 
   const db = new Database(':memory:')
   const JEDI = 'jedi'
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,101 @@
-type Target<T> = T | Awaited<T> | Iterable<T> | AsyncIterable<T>
+import { Database } from 'bun:sqlite'
+import { uuidv7 } from '@daaku/uuidv7'
+import memize from 'memize'
 
-export async function* flat<T>(t: Target<T | Target<T>>): AsyncGenerator<T> {
-  // await the promise, if any
-  let v = await t
+export const qCreateTable = memize(
+  (name: string): string => `create table if not exists ${name} (data text)`,
+)
 
-  if (v && typeof v === 'object') {
-    if (Symbol.asyncIterator in v) {
-      for await (const sub of v) {
-        yield* flat(sub)
-      }
-      return
-    }
+const exprToName = memize((expr: string): string =>
+  expr.replace(/[^a-zA-Z]+/g, '_').replace(/_$/, ''),
+)
 
-    if (Symbol.iterator in v) {
-      for (const sub of v) {
-        yield* flat(sub)
-      }
-      return
-    }
-  }
-
-  // otherwise itself
-  yield v
+export interface CreateIndex {
+  table: string
+  expr: string
+  name?: string
+  unique?: boolean
 }
+export const qCreateIndex = memize((o: CreateIndex): string => {
+  return [
+    'create ',
+    o.unique ? 'unique ' : '',
+    'index if not exists ',
+    o.name ?? `${o.table}_${exprToName(o.expr)}`,
+    ' on ',
+    o.table,
+    ' (',
+    o.expr,
+    ')',
+  ].join('')
+})
+
+const qInsert = memize(
+  (table: string): string => `insert into ${table} (data) values (?)`,
+)
+
+export const insert = <T extends Object>(
+  db: Database,
+  table: string,
+  doc: T,
+): T & { id: string } => {
+  // @ts-ignore muck with id
+  if (!doc.id) {
+    doc = { ...doc, id: uuidv7() }
+  }
+  const sql = qInsert(table)
+  const stmt = db.query<undefined, string>(sql)
+  stmt.run(JSON.stringify(doc))
+  // @ts-ignore muck with id
+  return doc
+}
+
+export const $toData = memize((s: string): string =>
+  s.replace(/\$([A-Za-z_\.]+)/g, `data->>'$1'`),
+)
+
+const $arrayToData = memize((vs: readonly string[]): string[] =>
+  vs.map($toData),
+)
+
+export interface SQLParts {
+  parts: readonly string[]
+  values: any[]
+}
+export const sql = (
+  strings: TemplateStringsArray,
+  ...values: any[]
+): SQLParts => {
+  return {
+    parts: $arrayToData(strings),
+    values: values,
+  }
+}
+
+export const all = <Doc = unknown>(
+  db: Database,
+  table: string,
+  ...sqls: SQLParts[]
+): Doc[] => {
+  const sql = [
+    'select data from',
+    table,
+    ...sqls.map(v => v.parts.join('?')),
+  ].join(' ')
+  const args = sqls.flatMap(v => v.values)
+  const stmt = db.query<{ data: string }, any[]>(sql)
+  // @ts-ignore we expect [string][]
+  return stmt.values(...args).flatMap(JSON.parse)
+}
+
+export const get = <Doc = unknown>(
+  db: Database,
+  table: string,
+  ...sqls: SQLParts[]
+): Doc | undefined => all<Doc>(db, table, ...sqls, sql`limit 1`)[0]
+
+export const getByID = <Doc = unknown>(
+  db: Database,
+  table: string,
+  id: string,
+): Doc | undefined => get<Doc>(db, table, sql`where $id = ${id}`)
